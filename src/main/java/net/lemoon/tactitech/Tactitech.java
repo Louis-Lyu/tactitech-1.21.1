16dbc6e49bc52fa5915420cfb03f44ea0194ffd7
Louis Lyu <louislyu@Louiss-Macbook-M4.local>
2025-06-18 19:54:03 +0800

    Initial commit

---
+++ src/main/java/net/lemoon/tactitech/Tactitech.java
@@ -0,0 +1,16 @@
+package net.lemoon.tactitech;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tactitech implements ModInitializer {
+	public static final String MOD_ID = "tactitech";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lemoon/tactitech/TactitechClient.java
@@ -0,0 +1,10 @@
+package net.lemoon.tactitech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TactitechClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
